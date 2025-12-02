7c09195f340960d8cb8416a2c845e157c6f18f0d
dynamo-mannal <133088089+dynamomanal@users.noreply.github.com>
2025-12-02 22:08:10 +0500

    create

---
+++ app/(screens)/contact/page.tsx
@@ -0,0 +1,10 @@
+const Contact:React.FC=()=>{
+    return (
+        <>
+        <div>
+            <h1 className=" text-white ">Contact Page</h1>
+        </div>
+        </>
+    )
+};
+export default Contact;
