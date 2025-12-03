474c351d3678b372cc0bc58644a13791ca53f604
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-12-03 18:18:32 +0500

    Hero Done

--- app/(screens)/case-studies/page.tsx
+++ app/(screens)/case-studies/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from '@/app/components/common/hero';
 import React from 'react'
 
 const CaseStudies = () => {
     return (
         <div>
-
+            <HeroSection />
         </div>
     )
 }
