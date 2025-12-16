50a1f9310d6f43edf378ad7855be7bcb56cccb8b
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-12-16 12:00:50 +0500

    Props

---
+++ app/(screens)/brand-experience/page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "@/app/components/common/hero";
+
+const BrandExperience = () => {
+    return (
+        <div>
+            <HeroSection />
+        </div>
+    )
+}
+
+export default BrandExperience;
--- app/components/common/hero.tsx
+++ app/components/common/hero.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 
-export default function HeroSection() {
+export default function HeroSection({ h1, h2, p }: { h1?: string; h2?: string; p?: string }) {
     return (
         <section className="bg-[#031347] min-h-[600px] flex items-center justify-center relative py-16">
             {/* Pizza image - left side */}
@@ -15,11 +15,10 @@ export default function HeroSection() {
 
             {/* Center content */}
             <div className="text-center z-10">
-                <h1 className="text-white text-[80px] font-fks font-bold uppercase tracking-wide">Real Projects</h1>
-                <h2 className="text-[#BBFC00] text-[120px] font-fks font-bold uppercase tracking-wide leading-20">Real Impact</h2>
+                <h1 className="text-white text-[80px] font-fks font-bold uppercase tracking-wide">{h1 ? h1 : "Real Projects"}</h1>
+                <h2 className="text-[#BBFC00] text-[120px] font-fks font-bold uppercase tracking-wide leading-20">{h2 ? h2 : "Real Impact"} </h2>
                 <p className="text-white/80 text-md mt-6 max-w-sm mx-auto">
-                    Explore how brands used SpawnPoint to
-                    enter the gaming universe
+                    {p ? p : "Explore how brands used SpawnPoint to enter the gaming universe"}
                 </p>
                 <button className="mt-6 bg-[#BBFC00] text-[#031347] font-semibold text-ms px-8 py-2.5 rounded-[5px] hover:bg-[#a8e600] transition-colors">
                     Lets Connect
