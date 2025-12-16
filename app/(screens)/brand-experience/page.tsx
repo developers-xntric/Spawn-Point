436044275ed5b171579483fe8edc0680b0de1cc5
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-12-16 12:03:57 +0500

    Pacman Burger

--- app/(screens)/brand-experience/page.tsx
+++ app/(screens)/brand-experience/page.tsx
@@ -3,7 +3,13 @@ import HeroSection from "@/app/components/common/hero";
 const BrandExperience = () => {
     return (
         <div>
-            <HeroSection />
+            <HeroSection
+                h1="Where brands"
+                h2="meet play"
+                p="We design campaigns, creators, esports moments, and virtual worlds that move culture and performance at the same time."
+                img1="/icons/pacman.png"
+                img2="/icons/burger.png"
+            />
         </div>
     )
 }
