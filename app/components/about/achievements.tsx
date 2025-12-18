f7895a01438c59aa83405b5428ff131575fbcb9d
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2025-12-18 15:44:34 +0500

    updated

--- app/components/about/achievements.tsx
+++ app/components/about/achievements.tsx
@@ -11,22 +11,22 @@ export function Achievements() {
           </p>
           <h2 className="text-[40px] lg:text-[60px] font-fks font-bold uppercase text-white max-w-[570px] mx-auto text-center leading-[40px] md:leading-[45px] lg:leading-[65px] text-balance tracking-[1.5px]">
             WE DON'T <span className="text-[#BBFC00]">CREATE ADS </span> WE BUILD WORLDS THAT WIN{" "}
-            <span className="text-[#BBFC00]">ATTENTION</span>.
+            <span className="text-[#BBFC00]">ATTENTION</span>
           </h2>
         </div>
 
         {/* Main Content Grid */}
-        <div className="grid items-center gap-6 md:gap-2 grid-cols-1 lg:grid-cols-3 xl:gap-0 place-items-center justify-between">
+        <div className="grid items-center gap-6 md:gap-2 grid-cols-1 lg:grid-cols-3 xl:gap0 xl:gap-6 place-items-center justify-between">
           {/* Left Stats Cards */}
-          <div className="space-y-6 flex flex-col w-full">
+          <div className="space-y-6 flex flex-col w-full h-full justify-between">
             {/* Card 1 */}
-            <div className="rounded-[12px] md:rounded-lg border border-[#FFFFFF1A] backdrop-blur-[30px] bg-white/5 pt-4 md:pt-10 pb-4 pl-4 ">
+            <div className="rounded-[12px] md:rounded-lg border border-[#FFFFFF1A] backdrop-blur-[30px] bg-white/5 pt-4 md:pt-10 xl:pt-10 2xl:pt-20 pb-4 pl-4 ">
               <div className="text-[40px] md:text-[55px] font-fks font-bold uppercase text-white max-w-[570px] mx-auto  leading-[55px] text-balance">#1</div>
               <p className="text-white text-[16px] md:text-[16px]  max-w-sm ">in branded activations on Roblox</p>
             </div>
 
             {/* Card 2 */}
-            <div className="rounded-[12px] md:rounded-lg border border-[#FFFFFF1A] backdrop-blur-[30px] bg-white/5 pt-5 md:pt-10 pb-4 pl-4 ">
+            <div className="rounded-[12px] md:rounded-lg border border-[#FFFFFF1A] backdrop-blur-[30px] bg-white/5 pt-5 md:pt-10 xl:pt-10 2xl:pt-20 pb-4 pl-4 ">
               <div className="text-[40px] md:text-[55px] font-fks font-bold uppercase text-white max-w-[570px] mx-auto  leading-[55px] text-balance tracking-[1.5px]">
                 2.5 BILLION<span className="text-[45px]">+</span>
               </div>
@@ -34,7 +34,7 @@ export function Achievements() {
             </div>
 
             {/* Card 3 */}
-            <div className="rounded-[12px] md:rounded-lg border border-[#FFFFFF1A] backdrop-blur-[30px] bg-white/5 pt-5 md:pt-10 pb-4 pl-4 ">
+            <div className="rounded-[12px] md:rounded-lg border border-[#FFFFFF1A] backdrop-blur-[30px] bg-white/5 pt-5 md:pt-10 xl:pt-10 2xl:pt-20 pb-4 pl-4 ">
               <div className="text-[40px] md:text-[55px] font-fks font-bold uppercase text-white max-w-[570px] mx-auto  leading-[55px] text-balance tracking-[1.5px]">Award Winning</div>
               <p className="text-white text-[16px] md:text-[16px]  max-w-sm">Recognized globally for excellence in brand activations</p>
             </div>
@@ -45,12 +45,12 @@ export function Achievements() {
             <Image
               src="/about/achievements.png"
               alt="Roblox 3D Icon"
-              fill
-              className="object-contain w-full"
+              width={2000}
+              height={2000}
+              className="object-contain w-[100%] h-full mx-auto"
               priority
             />
           </div>
-         
 
           {/* Right Info Card */}
           <div className="w-full rounded-lg h-full border border-[#FFFFFF1A] backdrop-blur-[30px] bg-white/5 p-4 md:p-8 flex flex-col justify-end space-y-6 md:pt-8 pt-6">
