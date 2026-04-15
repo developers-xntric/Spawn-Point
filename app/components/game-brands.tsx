08dbf7ff08ecd4270f38651d7c0f167ab1a34e4a
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-04-15 12:30:43 +0500

    Game Brands

--- app/components/game-brands.tsx
+++ app/components/game-brands.tsx
@@ -50,8 +50,8 @@ export default function GameBrands() {
     <div className="relative w-full overflow-hidden pt-6 md:pt-3 pb-1 md:pb-16">
       {/* Header */}
       <div className="relative z-10 text-center mb-10 md:mb-8">
-        <h2 className="text-[40px] lg:text-[60px] font-fks font-bold uppercase text-white max-w-[90%] md:max-w-[700px] mx-auto text-center leading-10 md:leading-[45px] lg:leading-[65px] tracking-[1.5px]">
-          The <span className="text-[#BBFC00]">Strategic Entry </span> Point to MENA's Fastest-Growing<span className="text-[#BBFC00]"> Audience</span>
+        <h2 className="text-[40px] lg:text-[60px] font-fks font-bold uppercase text-white max-w-[90%] md:max-w-[450px] mx-auto text-center leading-10 md:leading-[45px] lg:leading-[65px] tracking-[1.5px]">
+          The <span className="text-[#BBFC00]">Architects </span> of the GCC Metaverse
         </h2>
       </div>
       {/* GREENISH LINE */}
